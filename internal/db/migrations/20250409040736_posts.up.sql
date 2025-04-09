89005893551727c229e8d6ee477684200c81756a
Arya Adinul Fadlan <adinul.fadlan04@gmail.com>
2025-04-09 13:09:56 +0700

    create a new user's post

--- internal/db/migrations/20250409040736_posts.up.sql
+++ internal/db/migrations/20250409040736_posts.up.sql
@@ -1,8 +1,11 @@
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
 CREATE TABLE IF NOT EXISTS posts (
     "id" UUID PRIMARY KEY,
     "user_id" UUID NOT NULL,
     "title" VARCHAR(20) NOT NULL,
     "content" TEXT NOT NULL,
+    "tags" VARCHAR(20)[] NOT NULL,
     "created_at" TIMESTAMPTZ,
     "updated_at" TIMESTAMPTZ,
     FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE ON UPDATE CASCADE
