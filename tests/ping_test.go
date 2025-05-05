06026e84f0ccbcc77867bc80c76abc51e93ebcf5
Arya Adinul Fadlan <adinul.fadlan04@gmail.com>
2025-05-05 11:00:15 +0700

    refactoring ping_test with table test

--- tests/ping_test.go
+++ tests/ping_test.go
@@ -2,99 +2,82 @@ package tests
 
 import (
 	"fmt"
-	"io"
 	"net/http"
 	"net/http/httptest"
 	"testing"
-	"time"
 
 	"github.com/google/uuid"
 	"github.com/stretchr/testify/assert"
 )
 
-func TestPing(t *testing.T) {
+func TestRateLimiter(t *testing.T) {
 	router := SetupTest()
-	request := httptest.NewRequest(http.MethodGet, "/v1/ping", nil)
-	recorder := httptest.NewRecorder()
-	router.ServeHTTP(recorder, request)
-	response := recorder.Result()
-	bodyBytes, _ := io.ReadAll(response.Body)
-	assert.Equal(t, http.StatusOK, response.StatusCode)
-	assert.Equal(t, "PONG", string(bodyBytes))
-	time.Sleep(time.Second)
-}
-
-func TestPingWithRateLimiter(t *testing.T) {
-	router := SetupTest()
-	for range 5 {
-		request := httptest.NewRequest(http.MethodGet, "/v1/ping", nil)
-		recorder := httptest.NewRecorder()
-		router.ServeHTTP(recorder, request)
-		response := recorder.Result()
-		assert.Equal(t, http.StatusOK, response.StatusCode)
+	tests := []struct {
+		name           string
+		expectedStatus int
+	}{
+		{name: "OK", expectedStatus: http.StatusOK},
+		{name: "OK", expectedStatus: http.StatusOK},
+		{name: "OK", expectedStatus: http.StatusOK},
+		{name: "OK", expectedStatus: http.StatusOK},
+		{name: "OK", expectedStatus: http.StatusOK},
+		{name: "Too Many Request", expectedStatus: http.StatusTooManyRequests},
 	}
-	failedRequest := httptest.NewRequest(http.MethodGet, "/v1/ping", nil)
-	failedRecorder := httptest.NewRecorder()
-	router.ServeHTTP(failedRecorder, failedRequest)
-	failedResponse := failedRecorder.Result()
-	assert.Equal(t, http.StatusTooManyRequests, failedResponse.StatusCode)
-	time.Sleep(time.Second)
-	successRequest := httptest.NewRequest(http.MethodGet, "/v1/ping", nil)
-	successRecorder := httptest.NewRecorder()
-	router.ServeHTTP(successRecorder, successRequest)
-	successResponse := successRecorder.Result()
-	assert.Equal(t, http.StatusOK, successResponse.StatusCode)
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			request := httptest.NewRequest(http.MethodGet, "/v1/ping", nil)
+			recorder := httptest.NewRecorder()
+			router.ServeHTTP(recorder, request)
+			response := recorder.Result()
+			assert.Equal(t, test.expectedStatus, response.StatusCode)
+		})
+	}
 }
 
-func TestBasicAuthorized(t *testing.T) {
+func TestBasicAuthentication(t *testing.T) {
 	router := SetupTest()
-	request := httptest.NewRequest(http.MethodGet, "/v1/basic", nil)
-	request.Header.Set("Content-Type", "application/json")
-	request.Header.Set("Authorization", "Basic YWRpbnVsOmFkaW51bDEyMw==")
-	recorder := httptest.NewRecorder()
-	router.ServeHTTP(recorder, request)
-	response := recorder.Result()
-	bodyBytes, _ := io.ReadAll(response.Body)
-	bodyString := string(bodyBytes)
-	assert.Equal(t, http.StatusOK, response.StatusCode)
-	assert.Equal(t, "Authenticated as Basic Authentication", bodyString)
+	tests := []struct {
+		name           string
+		base64         string
+		expectedStatus int
+	}{
+		{name: "Valid Base64", base64: "YWRpbnVsOmFkaW51bDEyMw==", expectedStatus: http.StatusOK},
+		{name: "Invalid Base64", base64: "Invalid Base64", expectedStatus: http.StatusUnauthorized},
+	}
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			request := httptest.NewRequest(http.MethodGet, "/v1/basic", nil)
+			request.Header.Set("Content-Type", "application/json")
+			request.Header.Set("Authorization", fmt.Sprintf("Basic %s", test.base64))
+			recorder := httptest.NewRecorder()
+			router.ServeHTTP(recorder, request)
+			response := recorder.Result()
+			assert.Equal(t, test.expectedStatus, response.StatusCode)
+		})
+	}
 }
 
-func TestBasicUnauthorized(t *testing.T) {
-	router := SetupTest()
-	request := httptest.NewRequest(http.MethodGet, "/v1/basic", nil)
-	request.Header.Set("Content-Type", "application/json")
-	request.Header.Set("Authorization", "Basic invalid base64")
-	recorder := httptest.NewRecorder()
-	router.ServeHTTP(recorder, request)
-	response := recorder.Result()
-	assert.Equal(t, http.StatusUnauthorized, response.StatusCode)
-}
-
-func TestBearerAuthorized(t *testing.T) {
+func TestBearerAuthentication(t *testing.T) {
 	router := SetupTest()
 	user_id, _ := uuid.Parse("50b466de-2de4-4e40-bdec-08270f23a8c8")
-	token, err := GenerateJWT(user_id.String())
-	assert.Nil(t, err)
-	request := httptest.NewRequest(http.MethodGet, "/v1/bearer", nil)
-	request.Header.Set("Content-Type", "application/json")
-	request.Header.Set("Authorization", fmt.Sprintf("Bearer %s", token))
-	recorder := httptest.NewRecorder()
-	router.ServeHTTP(recorder, request)
-	response := recorder.Result()
-	bodyBytes, _ := io.ReadAll(response.Body)
-	bodyString := string(bodyBytes)
-	assert.Equal(t, http.StatusOK, response.StatusCode)
-	assert.Equal(t, "Authenticated as Bearer Authentication", bodyString)
-}
-
-func TestBearerUnauthorized(t *testing.T) {
-	router := SetupTest()
-	request := httptest.NewRequest(http.MethodGet, "/v1/bearer", nil)
-	request.Header.Set("Content-Type", "application/json")
-	request.Header.Set("Authorization", "Bearer invalid token")
-	recorder := httptest.NewRecorder()
-	router.ServeHTTP(recorder, request)
-	response := recorder.Result()
-	assert.Equal(t, http.StatusUnauthorized, response.StatusCode)
+	token, _ := GenerateJWT(user_id.String())
+	tests := []struct {
+		name           string
+		token          string
+		expectedStatus int
+	}{
+		{name: "Valid Token", token: token, expectedStatus: http.StatusOK},
+		{name: "Invalid Token", token: "Invalid Token", expectedStatus: http.StatusUnauthorized},
+	}
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			request := httptest.NewRequest(http.MethodGet, "/v1/bearer", nil)
+			request.Header.Set("Content-Type", "application/json")
+			request.Header.Set("Authorization", fmt.Sprintf("Bearer %s", test.token))
+			recorder := httptest.NewRecorder()
+			router.ServeHTTP(recorder, request)
+			response := recorder.Result()
+			assert.Equal(t, test.expectedStatus, response.StatusCode)
+		})
+	}
 }
