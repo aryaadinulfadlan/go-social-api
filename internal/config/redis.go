9ad3bf475067d2376978bac189ed6402c0ff6351
Arya Adinul Fadlan <adinul.fadlan04@gmail.com>
2025-05-07 10:50:38 +0700

    improve dockerize

--- internal/config/redis.go
+++ internal/config/redis.go
@@ -3,17 +3,13 @@ package config
 import "github.com/aryaadinulfadlan/go-social-api/internal/env"
 
 type RedisConfig struct {
-	Addr     string
-	Password string
-	DB       int
-	Protocol int
+	Addr string
+	DB   int
 }
 
 func LoadRedisConfig() *RedisConfig {
 	return &RedisConfig{
-		Addr:     env.Envs.REDIS_ADDR,
-		Password: env.Envs.REDIS_PASSWORD,
-		DB:       env.Envs.REDIS_DB,
-		Protocol: env.Envs.REDIS_PROTOCOL,
+		Addr: env.Envs.REDIS_ADDR,
+		DB:   env.Envs.REDIS_DB,
 	}
 }
